ff134edb9faf9bce409382c00135548b9fe8f0cf
Anton <pentyugov@icloud.com>
2020-11-26 23:03:39 +0400

    add new Entities

--- src/main/java/com/crackermarket/app/entities/Product.java
+++ src/main/java/com/crackermarket/app/entities/Product.java
@@ -2,14 +2,52 @@ package com.crackermarket.app.entities;
 
 import com.crackermarket.app.core.BaseEntity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.List;
 
 @Entity
 @Table(name = "PRODUCT")
 public class Product extends BaseEntity {
     @Column(name = "CATEGORY_ID")
     private Category category;
+    @Column(name = "PRICE")
+    private BigDecimal price;
+    @OneToMany
+    @JoinColumn(name = "PARAMETER_ID")
+    private List<Parameter> parameters;
+    @Column(name = "COUNT")
+    private Integer count;
 
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public List<Parameter> getParameters() {
+        return parameters;
+    }
+
+    public void setParameters(List<Parameter> parameters) {
+        this.parameters = parameters;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
 }

c45086e778428007f2df8fbf252cc220a29f3123
Anton Pentyugov <apentyugov@webzavod.ru>
2020-11-27 15:35:18 +0400

    add css and js

---
+++ src/main/resources/assets/js/script.js
@@ -0,0 +1,9 @@
+function discardChanges() {
+    var cancel = confirm('Discard changes?');
+    return cancel;
+}
+
+function saveCategory() {
+    var save = confirm('Save category?');
+    return save;
+}

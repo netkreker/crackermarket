98d3bddf3b584f733352106d60bddfb4767e2894
Anton <pentyugov@icloud.com>
2020-11-28 15:15:55 +0400

    temp commit

--- src/main/java/com/crackermarket/app/controllers/MainController.java
+++ src/main/java/com/crackermarket/app/controllers/MainController.java
@@ -1,8 +1,5 @@
 package com.crackermarket.app.controllers;
 
-import com.crackermarket.app.entities.Category;
-import com.crackermarket.app.services.CategoryDAO;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 

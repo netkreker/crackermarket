35e03af47df55ee2a6bae90ffe906ed093053d87
Anton <pentyugov@icloud.com>
2020-11-29 20:16:38 +0400

    temp commit

--- src/main/java/com/crackermarket/app/entities/Admin.java
+++ src/main/java/com/crackermarket/app/entities/Admin.java
@@ -1,4 +1,21 @@
 package com.crackermarket.app.entities;
 
-public class Admin {
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ADMIN")
+public class Admin extends User {
+
+    public Category createCategory(){
+        return new Category();
+    }
+
+    public Boolean deleteCategory(){
+        return false;
+    }
+
+    public Boolean setProductNewCategory(){
+        return false;
+    }
 }

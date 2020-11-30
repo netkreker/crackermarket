764f4a637f879e0d3b0e220cae2567de601ca670
SobolevD <soboiev86rus@mail.ru>
2020-11-30 13:18:53 +0400

    set BaseEntity as Admin's parent

--- src/main/java/com/crackermarket/app/entities/Admin.java
+++ src/main/java/com/crackermarket/app/entities/Admin.java
@@ -1,11 +1,35 @@
 package com.crackermarket.app.entities;
 
+import com.crackermarket.app.core.BaseEntity;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "ADMIN")
-public class Admin extends User {
+public class Admin extends BaseEntity {
+
+    @Column(name = "USER_NAME")
+    protected String userName;
+
+    @Column(name = "PASSWORD")
+    protected String password;
+
+    @Column(name = "EMAIL")
+    protected String email;
+
+    public String getUserName() { return userName; }
+
+    public void setUserName(String userName) { this.userName = userName; }
+
+    public String getPassword() { return password; }
+
+    public void setPassword(String password) { this.password = password; }
+
+    public String getEmail() { return email; }
+
+    public void setEmail(String email) { this.email = email; }
 
     public Category createCategory(){
         return new Category();
